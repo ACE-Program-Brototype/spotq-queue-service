bfb8bcbf9ea8fa23f680096ce00db92eae1099f9
Ajex Joshy <ajexjoshyclass8@gmail.com>
2026-08-04 02:31:07 +0530

    feat: configure Pino logging with AsyncLocalStorage context for request/correlation IDs

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
-
+import { loggerMiddleware } from './presentation/middleware/index.js';
 import { router } from './presentation/routes/index.routes.js';
 
 const app = express();
 
+app.use(loggerMiddleware);
 app.use(helmet());
 app.use(cors());
 app.use(compression());
