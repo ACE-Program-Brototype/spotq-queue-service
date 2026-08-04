70a4f4706fa47ea2234e1f3c3edf66c42a9cdfdf
Ajex Joshy <ajexjoshyclass8@gmail.com>
2026-08-04 15:28:35 +0530

    refactor: remove DIRECT_DATABASE_URL completely

--- src/config/env.ts
+++ src/config/env.ts
@@ -17,7 +17,7 @@ export const config = {
 
 	database: {
 		url: env.DATABASE_URL,
-		directUrl: env.DIRECT_DATABASE_URL || env.DATABASE_URL,
+		directUrl: env.DATABASE_URL,
 	},
 
 	redis: {
--- src/config/validate-env.ts
+++ src/config/validate-env.ts
@@ -10,7 +10,6 @@ const envSchema = z.object({
 	LOG_LEVEL: z.enum(['trace', 'debug', 'info', 'warn', 'error', 'fatal']),
 
 	DATABASE_URL: z.string().url(),
-	DIRECT_DATABASE_URL: z.string().url().optional(),
 
 	REDIS_URL: z.string().url(),
 });
