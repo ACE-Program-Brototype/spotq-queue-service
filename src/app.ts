6def7173c09a1d10acacc7faa1ff41cb3af03a6a
Ajex Joshy <ajexjoshyclass8@gmail.com>
2026-08-09 07:21:40 +0530

    refactor: remove redundant cors, helmet, and compression middlewares in internal service

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,12 @@ import {
 	notFoundMiddleware,
 } from '@presentation/middleware/index.js';
 import { router } from '@presentation/routes/index.routes.js';
-import compression from 'compression';
-import cors from 'cors';
 import express from 'express';
-import helmet from 'helmet';
 
 const app = express();
 
 app.use(loggerMiddleware);
 app.use(metricsMiddleware);
-app.use(helmet());
-app.use(cors());
-app.use(compression());
 app.use(express.json());
 
 app.use('/', router);
